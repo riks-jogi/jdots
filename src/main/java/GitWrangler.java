f70da70246ab31b3608519d8213b492dd420a973
Richard Jõgi <riks.jogi@gmail.com>
2022-04-07 13:15:21 +0300

    Added auth pipeline and verification

--- src/main/java/GitWrangler.java
+++ src/main/java/GitWrangler.java
@@ -1,4 +1,5 @@
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LsRemoteCommand;
 import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -11,6 +12,8 @@ import java.io.IOException;
 public class GitWrangler {
     Repository localRepo;
     Git git;
+    private String username;
+    private String secret;
 
 
     GitWrangler(String localPath) throws IOException {
@@ -18,17 +21,38 @@ public class GitWrangler {
         git = new Git(localRepo);
     }
 
-    public boolean pullRemote(String user, String pass) throws GitAPIException {
-        PullResult result = git.pull().setCredentialsProvider(new UsernamePasswordCredentialsProvider(user, pass)).call();
+    public void auth(){
+        this.username = System.getenv("GIT_USER");
+        this.secret = System.getenv("GIT_SECRET");
+    }
+
+    public void auth(String user, String pass){
+        this.username = user;
+        this.secret = pass;
+    }
+
+    public boolean testAuth(){
+        LsRemoteCommand test = git.lsRemote();
+        test.setCredentialsProvider(new UsernamePasswordCredentialsProvider(this.username, this.secret));
+        try {
+            test.call();
+            return true;
+        } catch (GitAPIException e) {
+            return false;
+        }
+    }
+
+    public boolean pullRemote() throws GitAPIException {
+        PullResult result = git.pull().setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, secret)).call();
         return result.isSuccessful();
     }
 
-    public void addCommitPush(String message, String user, String pass) throws GitAPIException {
+    public void addCommitPush(String message) throws GitAPIException {
         git.add().addFilepattern(".").call();
         git.commit().setMessage(message).call();
         PushCommand pushCommand = git.push();
         pushCommand.setProgressMonitor(new SimpleMonitor());
-        pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(user, pass));
+        pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, secret));
         pushCommand.call();
     }
 }
