2fe00aa89d994ddcf4ff0f8c0055ac74bba4607f
Mihkel Martin Kasterpalu <mkasterpalu@gmail.com>
2022-04-07 14:31:26 +0300

    Merge and update

--- src/main/java/FileWrangler.java
+++ src/main/java/FileWrangler.java
@@ -9,40 +9,65 @@ import java.util.*;
 
 public class FileWrangler {
     private List<DotFile> dotfailid = new ArrayList<>();
+    private final String gitPath;
+    private final String databasePath;
 
     public void lisaDotfail(String pathJaNimi) throws Exception {
         File dotfailiInfo = new File(pathJaNimi);
-        this.dotfailid.add(new DotFile(dotfailiInfo.getName(), dotfailiInfo.getParent()));
+        this.dotfailid.add(new DotFile(dotfailiInfo.getName(), dotfailiInfo.getParent() + "/"));
     }
+
     public void eemaldaDotfail(String pathJaNimi) {
         File dotfailiInfo = new File(pathJaNimi);
         this.dotfailid.removeIf(file -> Objects.equals(file.getNimi(), dotfailiInfo.getName()));
     }
 
-    public List<DotFile> leiaUuendused(String gitPath) throws Exception {
+    public FileWrangler(String gitPath, String databasePath) throws Exception {
+        this.gitPath = gitPath;
+        this.databasePath = databasePath;
+        this.loeDatabaasistFailid(databasePath);
+    }
+
+    private void loeDatabaasistFailid(String databasePath) {
+        try {
+            Scanner dotfailiRead = new Scanner(new File(databasePath));
+
+            while (dotfailiRead.hasNextLine()) {
+                String[] failiInfo = dotfailiRead.nextLine().split(";");
+                this.dotfailid.add(new DotFile(failiInfo[0], failiInfo[1], failiInfo[2]));
+            }
+        } catch (FileNotFoundException ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<DotFile> leiaUuendused() throws Exception {
         List<DotFile> uuendusegaFailid = new ArrayList<>();
-        for (DotFile file: this.dotfailid) {
-            if (file.kontrolliMuudatusi(gitPath)) uuendusegaFailid.add(file);
+        for (DotFile file : this.dotfailid) {
+            if (file.kontrolliMuudatusi(this.gitPath)) uuendusegaFailid.add(file);
         }
 
         return uuendusegaFailid;
     }
 
-    public void salvestaNimedJaChecksumid(String checksumFail) throws IOException {
-        FileWriter fileWriter = new FileWriter(checksumFail);
+    public void salvestaNimedJaChecksumid() throws IOException {
+        FileWriter fileWriter = new FileWriter(this.databasePath);
         PrintWriter printWriter = new PrintWriter(fileWriter);
 
-        for (DotFile file: this.dotfailid) {
+        for (DotFile file : this.dotfailid) {
+            System.out.println(file);
             printWriter.print(file.toDataString());
         }
         printWriter.close();
     }
 
-    public void uuendaDotFailGit(DotFile dotfile, String gitPath) throws IOException {
-        dotfile.uuendaGiti(gitPath);
+    public void uuendaDotFailGit(DotFile dotfile) throws IOException {
+        dotfile.uuendaGiti(this.gitPath);
     }
-    public void uuendaDotFailKoahlik(DotFile dotfile, String gitPath) throws Exception {
-        dotfile.uuendaKohalikku(gitPath);
+
+    public void uuendaDotFailKoahlik(DotFile dotfile) throws Exception {
+        dotfile.uuendaKohalikku(this.gitPath);
         dotfile.uuendaChecksum();
     }
 
