71abacf591c0c40b034d22055126a4b7cd1a861e
Mihkel Martin Kasterpalu <mkasterpalu@gmail.com>
2022-05-25 14:03:12 +0300

    Removed unused variable in ServerThread

--- src/main/java/ServerThread.java
+++ src/main/java/ServerThread.java
@@ -2,8 +2,6 @@ public class ServerThread implements Runnable{
     private GitWrangler repo;
     private FileWrangler files;
 
-    private volatile boolean running = true;
-
     public ServerThread(GitWrangler repo, FileWrangler files) {
         this.repo = repo;
         this.files = files;
